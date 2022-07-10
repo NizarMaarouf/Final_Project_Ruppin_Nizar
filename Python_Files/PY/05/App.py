bca44cd0a1e4bdf6538236a3aa7ec509cf4546c8
Nizar <nizar.maarouf1974.nm@gmail.com>
2022-07-10 09:51:40 +0300

    by nizar maarouf 10-7-2022

---
+++ Python_Files/PY/05/App.py
@@ -0,0 +1,18 @@
+from tkinter import *
+# import warnings filter
+from warnings import simplefilter
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
+
+App = Tk()
+App.title('Does the word start with the HAMZET "Wasl or Gtaa"?')
+# App.iconbitmap('Niar Maarouf')
+App.geometry('500x450')
+
+
+App.pack()
+
+
+
+
+App.mainloop()
